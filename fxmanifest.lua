b98a2d313d3e1733887c37a0dc43628122327a48
M-DEVELOPMENT <69606982+M-DEVELOPMENT23@users.noreply.github.com>
2024-01-28 18:47:32 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,30 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'true'
+author 'MANU - MANU5895'
+description 'Manu-JobNotification'
+
+client_scripts {
+    'client/*.lua',
+    'shared/*.lua',
+}
+
+shared_scripts {
+    'shared/*.lua',
+    '@ox_lib/init.lua',
+
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/*.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/index.js',
+    'html/*.jpg',
+}
